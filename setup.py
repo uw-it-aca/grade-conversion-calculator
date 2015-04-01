f6c35f6d2d8cf7c1189b7ad80ec1210567ffda94
Jim Laney <jlaney@uw.edu>
2015-04-01 16:18:40 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='grade-conversion-calculator',
+      version='1.0',
+      description='Grade Scale Conversion Calculator',
+      install_requires=['Django'],
+     )
