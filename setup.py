305d09ecb06bc889ce193d2c855ed851c6688464
Jim Laney <jlaney@uw.edu>
2015-04-02 12:45:21 -0700

    adding license

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from distutils.core import setup
 
 setup(name='grade-conversion-calculator',
       version='1.0',
+      license = 'Apache 2.0',
+      author = 'UW-IT ACA',
+      url='https://github.com/uw-it-aca/grade-conversion-calculator',
       description='Grade Scale Conversion Calculator',
       install_requires=['Django'],
      )
