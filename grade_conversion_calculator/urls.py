fad52cb91addcf4f3bc2b5cb689f74974287edd5
Jim Laney <jlaney@uw.edu>
2015-06-12 10:46:13 -0700

    pep8 fix

--- grade_conversion_calculator/urls.py
+++ grade_conversion_calculator/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^demo/?$', 'grade_conversion_calculator.views.demo'),
 )
